4fe0e3566678e07aada58db9d31149e056c147f4
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-07-24 18:52:24 -0500

    move to regex

--- samtools_metrics_sqlite/metrics/samtools_flagstat.py
+++ samtools_metrics_sqlite/metrics/samtools_flagstat.py
@@ -1,31 +1,31 @@
 import os
+import re
 
 import pandas as pd
 
-def samtools_flagstat_to_dict(uuid, metric_path, logger):
-    data_dict = dict()
-    values_to_store = ['in total', 'mapped', 'paired in sequencing', 'read1', 'read2']
-    with open(flagstat_path, 'r') as f_open:
+def samtools_flagstat_to_df(uuid, metric_path, logger):
+    columns = ['value_1', 'value_2', 'stat']
+    df = pd.DataFrame(columns=columns)
+    flagstat_re = re.compile("(^[\d]+) [\+] ([\d]+) (.*)$")
+    with open(metric_path, 'r') as f_open:
         for line in f_open:
             line = line.strip('\n')
-            for value_to_store in values_to_store:
-                if value_to_store in line:
-                    if value_to_store == 'mapped':
-                        if 'mate' in line:  # avoid 'with mate mapped to a different chr'/'with itself and mate mapped'
-                            continue
-                    line_split = line.split(' ')
-                    first_val = line_split[0]
-                    second_val = line_split[2]
-                    total_val = str(int(first_val) + int(second_val))
-                    data_dict[value_to_store] = total_val
-    return data_dict
+            line = line.strip()
+            flagstat_match = flagstat_re.match(line)
+            if flagstat_match is None:
+                sys.exit('bad match on line: %s' % line)
+            else:
+                value_1 = flagstat_match.groups()[0]
+                value_2 = flagstat_match.groups()[1]
+                stat = flagstat_match.groups()[2]
+                df.loc[len(df)] = [value_1, value_2, stat]
+    return df
 
 def run(uuid, metric_path, bam, input_state, engine, logger):
-    data_dict = samtools_flagstat_to_dict(uuid, bam_path, flagstat_path, logger)
-    data_dict['uuid'] = [uuid]
-    data_dict['bam'] = bam
-    data_dict['input_state'] = input_state
-    df = pd.DataFrame(data_dict)
+    df = samtools_flagstat_to_df(uuid, metric_path, logger)
+    df['uuid'] = uuid
+    df['bam'] = bam
+    df['input_state'] = input_state
     table_name = 'samtools_flagstat'
     df.to_sql(table_name, engine, if_exists='append')
     return
