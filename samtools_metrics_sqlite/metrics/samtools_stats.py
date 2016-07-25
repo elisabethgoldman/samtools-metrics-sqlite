6fbfd9677f9cca5a2a95f4099bc9d138e3f500c1
Jeremiah H. Savage <jeremiahsavage@gmail.com>
2016-07-24 19:38:19 -0500

    works

--- samtools_metrics_sqlite/metrics/samtools_stats.py
+++ samtools_metrics_sqlite/metrics/samtools_stats.py
@@ -1,15 +1,20 @@
 import pandas as pd
 
-def samtools_stats_to_dict(metric_path:
+def samtools_stats_to_dict(metric_path, uuid):
     data_dict = dict()
-    values_to_store = ['raw total sequences:', 'filtered sequences:', 'sequences:', 'is sorted:', '1st fragments:',
-                     'last fragments:', 'reads mapped:', 'reads mapped and paired:', 'reads unmapped:',
-                     'reads properly paired:', 'reads paired:', 'reads duplicated:', 'reads MQ0:', 'reads QC failed:',
-                     'non-primary alignments:', 'total length:', 'bases mapped:', 'bases mapped (cigar):', 'bases trimmed:',
-                     'bases duplicated:', 'mismatches:', 'error rate:', 'average length:', 'maximum length:', 'average quality:',
-                     'insert size average:', 'insert size standard deviation:', 'inward oriented pairs:',
-                     'outward oriented pairs:', 'pairs with other orientation:', 'pairs on different chromosomes:']
-    with open(stats_file, 'r') as f_open:
+    data_dict['uuid'] = [uuid]
+    values_to_store = ['raw total sequences:', 'filtered sequences:', 'sequences:',
+                       'is sorted:', '1st fragments:', 'last fragments:', 'reads mapped:',
+                       'reads mapped and paired:', 'reads unmapped:',
+                       'reads properly paired:', 'reads paired:', 'reads duplicated:',
+                       'reads MQ0:', 'reads QC failed:', 'non-primary alignments:',
+                       'total length:', 'bases mapped:', 'bases mapped (cigar):',
+                       'bases trimmed:', 'bases duplicated:', 'mismatches:', 'error rate:',
+                       'average length:', 'maximum length:', 'average quality:',
+                       'insert size average:', 'insert size standard deviation:',
+                       'inward oriented pairs:', 'outward oriented pairs:',
+                       'pairs with other orientation:', 'pairs on different chromosomes:']
+    with open(metric_path, 'r') as f_open:
         for line in f_open:
             line = line.strip('\n')
             if line.startswith('SN\t'):
@@ -26,9 +31,10 @@ def samtools_stats_to_dict(metric_path:
 
 
 def run(uuid, metric_path, bam, input_state, engine, logger):
-    data_dict = samtools_stats_to_dict(metric_path)
+    data_dict = samtools_stats_to_dict(metric_path, uuid)
+    print('data_dict=\n%s' % data_dict)
     df = pd.DataFrame(data_dict)
-    df['uuid'] = uuid
+    #df['uuid'] = [uuid]
     df['bam'] = bam
     df['input_state'] = input_state
     table_name = 'samtools_stats'
